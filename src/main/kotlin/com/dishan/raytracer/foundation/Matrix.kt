b3034029f569114c9ef22f42bf3fb6323aaad0a2
Dishan <dishanf@irononetech.com>
2023-04-10 10:54:06 +0530

    Add mroe matrix creation methods

--- src/main/kotlin/com/dishan/raytracer/foundation/Matrix.kt
+++ src/main/kotlin/com/dishan/raytracer/foundation/Matrix.kt
@@ -4,6 +4,12 @@ import com.dishan.raytracer.util.closeEnough
 
 open class Matrix(val rows: Int, val cols: Int) {
 
+    constructor(rows: Int, cols: Int, vararg elements: Float) : this(rows, cols) {
+        for (i in elements.indices) {
+            this.elements[i] = elements[i]
+        }
+    }
+
     companion object {
 
         fun create(rows: Int, cols: Int, init: (Int, Int) -> Float): Matrix {
@@ -25,7 +31,7 @@ open class Matrix(val rows: Int, val cols: Int) {
                 && (this.elements zip other.elements).all { (a, b) -> a.closeEnough(b) }
     }
 
-    infix fun `~!=`(other: Matrix): Boolean =  !(this `~==` other)
+    infix fun `~!=`(other: Matrix): Boolean = !(this `~==` other)
 
     operator fun get(row: Int, col: Int): Float = elements[row * cols + col]
 
@@ -34,19 +40,20 @@ open class Matrix(val rows: Int, val cols: Int) {
     }
 }
 
-class Matrix4 : Matrix(4, 4) {
+class Matrix4(vararg elements: Float) : Matrix(4, 4, *elements) {
+
     companion object {
         fun create(init: (Int, Int) -> Float) = create(4, 4, init)
     }
 }
 
-class Matrix2 : Matrix(2, 2) {
+class Matrix2(vararg elements: Float) : Matrix(2, 2, *elements) {
     companion object {
         fun create(init: (Int, Int) -> Float) = create(2, 2, init)
     }
 }
 
-class Matrix3 : Matrix(3, 3) {
+class Matrix3(vararg elements: Float) : Matrix(3, 3, *elements) {
     companion object {
         fun create(init: (Int, Int) -> Float) = create(3, 3, init)
     }
--- src/test/kotlin/com/dishan/raytracer/foundation/MatrixTest.kt
+++ src/test/kotlin/com/dishan/raytracer/foundation/MatrixTest.kt
@@ -10,10 +10,22 @@ class MatrixTest {
     fun `Constructing and inspecting a 4x4 matrix`() {
         val matrix = Matrix4.create { row, col ->
             when (row to col) {
-                0 to 0 -> 1.0f 0 to 1 -> 2.0f 0 to 2 -> 3.0f 0 to 3 -> 4.0f
-                1 to 0 -> 5.5f 1 to 1 -> 6.5f 1 to 2 -> 7.5f 1 to 3 -> 8.5f
-                2 to 0 -> 9.0f 2 to 1 -> 10.0f 2 to 2 -> 11.0f 2 to 3 -> 12.0f
-                3 to 0 -> 13.5f 3 to 1 -> 14.5f 3 to 2 -> 15.5f 3 to 3 -> 16.5f
+                0 to 0 -> 1.0f
+                0 to 1 -> 2.0f
+                0 to 2 -> 3.0f
+                0 to 3 -> 4.0f
+                1 to 0 -> 5.5f
+                1 to 1 -> 6.5f
+                1 to 2 -> 7.5f
+                1 to 3 -> 8.5f
+                2 to 0 -> 9.0f
+                2 to 1 -> 10.0f
+                2 to 2 -> 11.0f
+                2 to 3 -> 12.0f
+                3 to 0 -> 13.5f
+                3 to 1 -> 14.5f
+                3 to 2 -> 15.5f
+                3 to 3 -> 16.5f
                 else -> error("Invalid element index: ($row, $col)")
             }
         }
@@ -28,11 +40,31 @@ class MatrixTest {
     }
 
     @Test
+    fun `Constructing and inspecting a 4x4 matrix method 2`() {
+        val matrix = Matrix4(
+            1f, 2f, 3f, 4f,
+            5.5f, 6.5f, 7.5f, 8.5f,
+            9f, 10f, 11f, 12f,
+            13.5f, 14.5f, 15.5f, 16.5f,
+        )
+
+        assertEquals(1f, matrix[0, 0])
+        assertEquals(4f, matrix[0, 3])
+        assertEquals(5.5f, matrix[1, 0])
+        assertEquals(7.5f, matrix[1, 2])
+        assertEquals(11f, matrix[2, 2])
+        assertEquals(13.5f, matrix[3, 0])
+        assertEquals(15.5f, matrix[3, 2])
+    }
+
+    @Test
     fun `A 2x2 matrix ought to be representable`() {
         val matrix = Matrix2.create { row, col ->
             when (row to col) {
-                0 to 0 -> -3f 0 to 1 -> 5f
-                1 to 0 -> 1f 1 to 1 -> -2f
+                0 to 0 -> -3f
+                0 to 1 -> 5f
+                1 to 0 -> 1f
+                1 to 1 -> -2f
                 else -> error("Invalid element index: ($row, $col)")
             }
         }
@@ -47,9 +79,15 @@ class MatrixTest {
     fun `A 3x3 matrix ought to be representable`() {
         val matrix = Matrix3.create { row, col ->
             when (row to col) {
-                0 to 0 -> -3f 0 to 1 -> 5f 0 to 2 -> 0f
-                1 to 0 -> 1f 1 to 1 -> -2f 1 to 2 -> -7f
-                2 to 0 -> 0f 2 to 1 -> 1f 2 to 2 -> 1f
+                0 to 0 -> -3f
+                0 to 1 -> 5f
+                0 to 2 -> 0f
+                1 to 0 -> 1f
+                1 to 1 -> -2f
+                1 to 2 -> -7f
+                2 to 0 -> 0f
+                2 to 1 -> 1f
+                2 to 2 -> 1f
                 else -> error("Invalid element index: ($row, $col)")
             }
         }
@@ -63,20 +101,44 @@ class MatrixTest {
     fun `Matrix equality with identical matrices`() {
         val a = Matrix4.create { row, col ->
             when (row to col) {
-                0 to 0 -> 1f 0 to 1 -> 2f 0 to 2 -> 3f 0 to 3 -> 4f
-                1 to 0 -> 5f 1 to 1 -> 6f 1 to 2 -> 7f 1 to 3 -> 8f
-                2 to 0 -> 9f 2 to 1 -> 8f 2 to 2 -> 7f 2 to 3 -> 6f
-                3 to 0 -> 5f 3 to 1 -> 4f 3 to 2 -> 3f 3 to 3 -> 2f
+                0 to 0 -> 1f
+                0 to 1 -> 2f
+                0 to 2 -> 3f
+                0 to 3 -> 4f
+                1 to 0 -> 5f
+                1 to 1 -> 6f
+                1 to 2 -> 7f
+                1 to 3 -> 8f
+                2 to 0 -> 9f
+                2 to 1 -> 8f
+                2 to 2 -> 7f
+                2 to 3 -> 6f
+                3 to 0 -> 5f
+                3 to 1 -> 4f
+                3 to 2 -> 3f
+                3 to 3 -> 2f
                 else -> error("Invalid element index: ($row, $col)")
             }
         }
 
         val b = Matrix4.create { row, col ->
             when (row to col) {
-                0 to 0 -> 1f 0 to 1 -> 2f 0 to 2 -> 3f 0 to 3 -> 4f
-                1 to 0 -> 5f 1 to 1 -> 6f 1 to 2 -> 7f 1 to 3 -> 8f
-                2 to 0 -> 9f 2 to 1 -> 8f 2 to 2 -> 7f 2 to 3 -> 6f
-                3 to 0 -> 5f 3 to 1 -> 4f 3 to 2 -> 3f 3 to 3 -> 2f
+                0 to 0 -> 1f
+                0 to 1 -> 2f
+                0 to 2 -> 3f
+                0 to 3 -> 4f
+                1 to 0 -> 5f
+                1 to 1 -> 6f
+                1 to 2 -> 7f
+                1 to 3 -> 8f
+                2 to 0 -> 9f
+                2 to 1 -> 8f
+                2 to 2 -> 7f
+                2 to 3 -> 6f
+                3 to 0 -> 5f
+                3 to 1 -> 4f
+                3 to 2 -> 3f
+                3 to 3 -> 2f
                 else -> error("Invalid element index: ($row, $col)")
             }
         }
@@ -88,20 +150,44 @@ class MatrixTest {
     fun `Matrix equality with different matrices`() {
         val a = Matrix4.create { row, col ->
             when (row to col) {
-                0 to 0 -> 1f 0 to 1 -> 2f 0 to 2 -> 3f 0 to 3 -> 4f
-                1 to 0 -> 5f 1 to 1 -> 6f 1 to 2 -> 7f 1 to 3 -> 8f
-                2 to 0 -> 9f 2 to 1 -> 8f 2 to 2 -> 7f 2 to 3 -> 6f
-                3 to 0 -> 5f 3 to 1 -> 4f 3 to 2 -> 3f 3 to 3 -> 2f
+                0 to 0 -> 1f
+                0 to 1 -> 2f
+                0 to 2 -> 3f
+                0 to 3 -> 4f
+                1 to 0 -> 5f
+                1 to 1 -> 6f
+                1 to 2 -> 7f
+                1 to 3 -> 8f
+                2 to 0 -> 9f
+                2 to 1 -> 8f
+                2 to 2 -> 7f
+                2 to 3 -> 6f
+                3 to 0 -> 5f
+                3 to 1 -> 4f
+                3 to 2 -> 3f
+                3 to 3 -> 2f
                 else -> error("Invalid element index: ($row, $col)")
             }
         }
 
         val b = Matrix4.create { row, col ->
             when (row to col) {
-                0 to 0 -> 2f 0 to 1 -> 3f 0 to 2 -> 4f 0 to 3 -> 5f
-                1 to 0 -> 6f 1 to 1 -> 7f 1 to 2 -> 8f 1 to 3 -> 9f
-                2 to 0 -> 8f 2 to 1 -> 7f 2 to 2 -> 6f 2 to 3 -> 5f
-                3 to 0 -> 4f 3 to 1 -> 3f 3 to 2 -> 2f 3 to 3 -> 1f
+                0 to 0 -> 2f
+                0 to 1 -> 3f
+                0 to 2 -> 4f
+                0 to 3 -> 5f
+                1 to 0 -> 6f
+                1 to 1 -> 7f
+                1 to 2 -> 8f
+                1 to 3 -> 9f
+                2 to 0 -> 8f
+                2 to 1 -> 7f
+                2 to 2 -> 6f
+                2 to 3 -> 5f
+                3 to 0 -> 4f
+                3 to 1 -> 3f
+                3 to 2 -> 2f
+                3 to 3 -> 1f
                 else -> error("Invalid element index: ($row, $col)")
             }
         }
