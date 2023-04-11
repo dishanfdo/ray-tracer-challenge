0cda88551f702b617db3680a58a2aeafd4092862
Dishan <dishanf@irononetech.com>
2023-04-11 10:05:51 +0530

    Sample: Clock

--- src/main/kotlin/com/dishan/raytracer/foundation/Transformations.kt
+++ src/main/kotlin/com/dishan/raytracer/foundation/Transformations.kt
@@ -17,7 +17,7 @@ fun translation(x: Float = 0f, y: Float = 0f, z: Float = 0f): Matrix4 {
 
 fun scaling(x: Int = 0, y: Int = 0, z: Int = 0): Matrix4 = scaling(x.toFloat(), y.toFloat(), z.toFloat())
 
-fun scaling(x: Float, y: Float, z: Float): Matrix4 {
+fun scaling(x: Float = 0f, y: Float = 0f, z: Float = 0f): Matrix4 {
     return Matrix4.identity.apply {
         this[0, 0] = x
         this[1, 1] = y
---
+++ src/main/kotlin/com/dishan/raytracer/samples/Clock.kt
@@ -0,0 +1,25 @@
+package com.dishan.raytracer.samples
+
+import com.dishan.raytracer.foundation.*
+import com.dishan.raytracer.output.toPPM
+import com.dishan.raytracer.samples.projectile.writeToFile
+import kotlin.math.PI
+
+fun main() {
+    val canvasSize = 200
+    val padding = 40
+    val clockRadius = (canvasSize - padding) / 2.0f
+
+    val canvas = Canvas(canvasSize, canvasSize)
+    val p = point(0, 1, 0)
+    val scaling = scaling(clockRadius, clockRadius)
+    val translation = translation(canvasSize / 2, canvasSize / 2)
+
+    for (i in 1 .. 12) {
+        val rotation = rotationZ((PI / 6) * i)
+        val point = translation * scaling * rotation * p
+        canvas[point.x.toInt(), point.y.toInt()] = Color.White
+    }
+
+    canvas.toPPM().writeToFile("./output/clock.ppm")
+}
